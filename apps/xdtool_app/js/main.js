28656c6513508b2d1f43fc3165471efa2fa4d604
YAO <kirayao@hotmail.com>
2020-05-23 23:05:35 +0800

    backup welcome page

--- apps/xdtool_app/js/main.js
+++ apps/xdtool_app/js/main.js
@@ -2,6 +2,9 @@ ofa = async () => {
     let [, ofaStorage] = await load(`comps/ele-frame -pack`, "js/ofaStorage");
     window.ofaStorage = ofaStorage;
 
+    // 设置公用css
+    ofa.globalcss = "css/global.css";
+
     // 慢行1秒
     setTimeout(() => {
         $("#startLoading").class.add("hide");
---
+++ apps/xdtool_app/pages/welcome/welcome.js
@@ -0,0 +1,8 @@
+define(async (load) => {
+    return {
+        temp: true,
+        css: true,
+        data: {},
+        async ready() { }
+    };
+});
